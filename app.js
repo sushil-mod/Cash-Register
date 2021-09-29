92342f4896b01caa309b4d922c99b078f7b03ff8
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2021-09-29 21:12:47 +0530

    added final logic

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ var billAmt = document.querySelector("#bill-amount");
 var cashAmt = document.querySelector("#cash-amount");
  var checkBtn = document.querySelector("#check-button");
  var message = document.querySelector("#error-message");
+ var changeToReturn = document.querySelectorAll(".list-of-notes");
+
+var notes = [2000,500,100,20,10,5,1];
 
 checkBtn.addEventListener("click", function validateAmount() {
     hideMsg();
     if(billAmt.value > 0){
         if(cashAmt.value>billAmt.value){
-            var returnAmt= cashAmt-billAmt;
+            var returnAmt= cashAmt.value-billAmt.value;
+           
             calculateChangeAmt(returnAmt);
 
         }else{
@@ -17,13 +21,20 @@ checkBtn.addEventListener("click", function validateAmount() {
         showMsg("Invalid amount");
     }
 });
+
+function calculateChangeAmt(returnAmt){
+
+    for(let i = 0 ;i < notes.length;i++){
+        var NotesToReturn = Math.trunc(returnAmt / notes[i]);
+        returnAmt = returnAmt % notes[i];
+
+        changeToReturn[i].innerText=NotesToReturn;
+    }
+}
 function hideMsg(){
 message.style.display="none";
 }
 function showMsg(msg){
     message.style.display = "block";
-    message.innerText = msg
-}
-function calculateChangeAmt(){
-    
+    message.innerText = msg;
 }
