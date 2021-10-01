277d7631d8ad6a940c0f6d8baa8faf50c6677ca8
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2021-10-01 18:14:29 +0530

    style : added new display feature

--- app.js
+++ app.js
@@ -3,22 +3,48 @@ var cashAmt = document.querySelector("#cash-amount");
  var checkBtn = document.querySelector("#check-button");
  var message = document.querySelector("#error-message");
  var changeToReturn = document.querySelectorAll(".list-of-notes");
+var nextBtn = document.querySelector("#next-button");
+
+
+var showCash = document.querySelector(".show-cash");
+var showchange = document.querySelector(".show-change");
+
+
 
 var notes = [2000,500,100,20,10,5,1];
 
+
+nextBtn.addEventListener('click', nextBtnHandler)
+
+
+function nextBtnHandler (){
+    if (billAmt.value > 0) {
+       nextBtn.style.display="none";
+       showCash.style.display="block";
+        
+        hideMsg();
+    }else {
+        showMsg("enter valid bill amount");
+    }
+
+}
+
+
+
 checkBtn.addEventListener("click", function validateAmount() {
-    hideMsg();
-    if(billAmt.value > 0){
-        if(cashAmt.value>billAmt.value){
+    
+    if(cashAmt.value > 0){
+        if(Number(cashAmt.value)> Number(billAmt.value)){
             var returnAmt= cashAmt.value-billAmt.value;
-           
+           showchange.style.display="block";
+           hideMsg();
             calculateChangeAmt(returnAmt);
 
         }else{
             showMsg("more amount required for bill");
         }
     }else{
-        showMsg("Invalid amount");
+        showMsg("Invalid cash amount");
     }
 });
 
